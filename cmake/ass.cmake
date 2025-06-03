8a1f80b10ecb202d601556c757ca4c4fb40980dc
peerless2012 <peerless2012@126.com>
2025-06-04 00:23:04 +0800

    Add fontconfig and expat.

--- cmake/ass.cmake
+++ cmake/ass.cmake
@@ -1,12 +1,12 @@
 cmake_minimum_required(VERSION 3.10)
 ExternalProject_Add(ep_ass
-        DEPENDS ep_harfbuzz ep_freetype ep_fribidi ep_unibreak ep_png
+        DEPENDS ep_fontconfig ep_harfbuzz ep_freetype ep_fribidi ep_unibreak ep_png
         SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
         SOURCE_SUBDIR "src/ass"
         INSTALL_DIR ${CMAKE_BINARY_DIR}
         CONFIGURE_COMMAND
-            PKG_CONFIG_PATH=${CMAKE_BINARY_DIR}/lib
-            FREETYPE_CFLAGS=-I${CMAKE_BINARY_DIR}/include/freetype2  FREETYPE_LIBS=-L${CMAKE_BINARY_DIR}/lib/libfreetype.a
+            PKG_CONFIG_PATH=${CMAKE_BINARY_DIR}/lib/pkgconfig
+            FREETYPE_CFLAGS=-I${CMAKE_BINARY_DIR}/include/freetype2  FREETYPE_LIBS=-L${CMAKE_BINARY_DIR}/lib
             FRIBIDI_CFLAGS=-I${CMAKE_BINARY_DIR}/include/fribidi  FRIBIDI_LIBS=-L${CMAKE_BINARY_DIR}/lib/libfribidi.a
             HARFBUZZ_CFLAGS=-I${CMAKE_BINARY_DIR}/include/harfbuzz  HARFBUZZ_LIBS=-L${CMAKE_BINARY_DIR}/lib/libharfbuzz.a
             LIBUNIBREAK_CFLAGS=-I${CMAKE_BINARY_DIR}/include  LIBUNIBREAK_LIBS=-L${CMAKE_BINARY_DIR}/libunibreak.a
@@ -16,8 +16,8 @@ ExternalProject_Add(ep_ass
             <SOURCE_DIR>/<SOURCE_SUBDIR>/configure
             ${PLATFORM_BUILD_AND_HOST} ${CONFIGURE_VERBOSE_ARG}
             --prefix=${CMAKE_BINARY_DIR} --enable-shared=false --enable-static=yes
+            --enable-fontconfig
             --disable-dependency-tracking --disable-require-system-font-provider
-            --disable-fontconfig
             --srcdir=<SOURCE_DIR>/<SOURCE_SUBDIR>
         BUILD_COMMAND ${MAKE_EXECUTABLE} ${MAKE_JFLAG}
 )
---
+++ cmake/fontconfig.cmake
@@ -0,0 +1,17 @@
+ExternalProject_Add(ep_fontconfig
+        DEPENDS ep_expat ep_freetype
+        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+        SOURCE_SUBDIR "src/fontconfig"
+        INSTALL_DIR ${CMAKE_BINARY_DIR}
+        CONFIGURE_COMMAND
+        PKG_CONFIG_PATH=${CMAKE_BINARY_DIR}/lib/pkgconfig
+        ${CMAKE_COMMAND} -E env ${PLATFORM_CONFIGURE_ENV}
+        <SOURCE_DIR>/<SOURCE_SUBDIR>/configure ${PLATFORM_BUILD_AND_HOST}
+        --prefix=<INSTALL_DIR>
+        --enable-static --disable-shared
+        --with-expat-includes=${CMAKE_BINARY_DIR}/include
+        --with-expat-lib=${CMAKE_BINARY_DIR}/lib
+        --with-freetype-config=${CMAKE_BINARY_DIR}/bin/freetype-config
+        BUILD_COMMAND make
+        INSTALL_COMMAND make install
+)
