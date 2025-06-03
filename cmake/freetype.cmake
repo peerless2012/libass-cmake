7d170ce2348d70796b6abf0f609a77de8e14c5a4
peerless2012 <peerless2012@126.com>
2025-06-03 23:46:41 +0800

    Disable freetype with d suffix.

--- cmake/freetype.cmake
+++ cmake/freetype.cmake
@@ -3,7 +3,7 @@ ExternalProject_Add(ep_freetype
         SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
         SOURCE_SUBDIR "src/freetype"
         INSTALL_DIR ${CMAKE_BINARY_DIR}
-        CMAKE_ARGS ${EXTERNAL_PROJECT_CMAKE_ARGS}
+        CMAKE_ARGS ${EXTERNAL_PROJECT_CMAKE_ARGS} -DDISABLE_FORCE_DEBUG_POSTFIX=ON
 )
 include_directories(${CMAKE_BINARY_DIR}/include)
 link_directories(${CMAKE_BINARY_DIR}/lib)
