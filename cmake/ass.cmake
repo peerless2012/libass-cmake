e3e67884fd0a288706b2a3a726df8697d2bf46b4
moi15moi <moi15moismokerlolilol@gmail.com>
2025-06-04 13:30:44 -0400

    Format all file so they have 4 spaces has tab

--- cmake/ass.cmake
+++ cmake/ass.cmake
@@ -1,21 +1,21 @@
 cmake_minimum_required(VERSION 3.10)
 ExternalProject_Add(ep_ass
-        DEPENDS ep_fontconfig ep_harfbuzz ep_freetype ep_fribidi ep_unibreak
-        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
-        SOURCE_SUBDIR "src/ass"
-        INSTALL_DIR ${CMAKE_BINARY_DIR}
-        CONFIGURE_COMMAND
-            ${CMAKE_COMMAND} -E env ${PLATFORM_CONFIGURE_ENV}
-            <SOURCE_DIR>/<SOURCE_SUBDIR>/configure
-            ${PLATFORM_BUILD_AND_HOST} ${CONFIGURE_VERBOSE_ARG}
-            --prefix=${CMAKE_BINARY_DIR}
-            --enable-shared=false
-            --enable-static=yes
-            --enable-libunibreak
-            --enable-fontconfig
-            --srcdir=<SOURCE_DIR>/<SOURCE_SUBDIR>
-        BUILD_COMMAND make
-        INSTALL_COMMAND make install
+    DEPENDS ep_fontconfig ep_harfbuzz ep_freetype ep_fribidi ep_unibreak
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+    SOURCE_SUBDIR "src/ass"
+    INSTALL_DIR ${CMAKE_BINARY_DIR}
+    CONFIGURE_COMMAND
+        ${CMAKE_COMMAND} -E env ${PLATFORM_CONFIGURE_ENV}
+        <SOURCE_DIR>/<SOURCE_SUBDIR>/configure
+        ${PLATFORM_BUILD_AND_HOST} ${CONFIGURE_VERBOSE_ARG}
+        --prefix=${CMAKE_BINARY_DIR}
+        --enable-shared=false
+        --enable-static=yes
+        --enable-libunibreak
+        --enable-fontconfig
+        --srcdir=<SOURCE_DIR>/<SOURCE_SUBDIR>
+    BUILD_COMMAND make
+    INSTALL_COMMAND make install
 )
 add_dependencies(ep_ass ep_freetype ep_harfbuzz ep_fribidi ep_unibreak)
 include_directories({CMAKE_BINARY_DIR}/include)
--- cmake/expat.cmake
+++ cmake/expat.cmake
@@ -1,13 +1,18 @@
 cmake_minimum_required(VERSION 3.10)
 ExternalProject_Add(ep_expat
-        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
-        SOURCE_SUBDIR "src/expat/expat"
-        INSTALL_DIR ${CMAKE_BINARY_DIR}
-        CONFIGURE_COMMAND
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+    SOURCE_SUBDIR "src/expat/expat"
+    INSTALL_DIR ${CMAKE_BINARY_DIR}
+    CONFIGURE_COMMAND
         ${CMAKE_COMMAND} -E env ${PLATFORM_CONFIGURE_ENV}
-        <SOURCE_DIR>/<SOURCE_SUBDIR>/configure ${PLATFORM_BUILD_AND_HOST} -prefix=<INSTALL_DIR> --enable-static --disable-shared --without-tests --without-docbook
-        BUILD_COMMAND make
-        INSTALL_COMMAND make install
+        <SOURCE_DIR>/<SOURCE_SUBDIR>/configure ${PLATFORM_BUILD_AND_HOST}
+        --prefix=<INSTALL_DIR>
+        --enable-static
+        --disable-shared
+        --without-tests
+        --without-docbook
+    BUILD_COMMAND make
+    INSTALL_COMMAND make install
 )
 include_directories(${CMAKE_BINARY_DIR}/include)
 link_directories(${CMAKE_BINARY_DIR}/lib)
