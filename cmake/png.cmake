000073ad3c3464e94459806e7ede0ef56dd0d750
peerless2012 <peerless2012@126.com>
2025-06-05 23:06:01 +0800

    Remove png.

--- cmake/png.cmake
+++
@@ -1,8 +0,0 @@
-ExternalProject_Add(ep_png
-        SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
-        SOURCE_SUBDIR "src/png"
-        INSTALL_DIR ${CMAKE_BINARY_DIR}
-        CMAKE_ARGS ${EXTERNAL_PROJECT_CMAKE_ARGS} -DPNG_SHARED=OFF
-)
-include_directories(${CMAKE_BINARY_DIR}/include)
-link_directories(${CMAKE_BINARY_DIR}/lib)
